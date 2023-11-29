fd41679194460404547b90a4f9d1ecb8bff8528c
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-11-29 09:31:32 +0100

    Add files via upload

---
+++ klaseIObjekti/klaseIObjekti/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseIObjekti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+
+            Console.WriteLine(x380.ToString());
+
+            Zrakoplov C162 = new Zrakoplov("Skyscratcher", 75, 870);
+
+            Console.WriteLine(C162.ToString());
+
+            Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+            Console.WriteLine(PC21.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ klaseIObjekti/klaseIObjekti/Zrakoplov.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseIObjekti
+{
+    internal class Zrakoplov
+    {
+        string Naziv;
+        double SnagaMotora;
+        int DosegLeta;
+
+        public void setNaziv(string Naziv)
+        {
+            this.Naziv = Naziv;
+        }
+
+        public string getNaziv()
+        {
+            return Naziv;   
+        }
+        public void getSnagaMotora(double SnagaMotora)
+        {
+            this.SnagaMotora = SnagaMotora;
+        }
+
+        public double getSnagaMotora()
+        {
+            return SnagaMotora;
+        }
+        public void getDosegLeta(int DosegLeta)
+        {
+            this.DosegLeta = DosegLeta; 
+        }
+
+        public int getDosegLeta()
+        {
+            return DosegLeta;
+        }
+
+        public Zrakoplov(string naziv, double snagaMotora, int dosegLeta)
+        {
+            Naziv = naziv;
+            SnagaMotora = snagaMotora;
+            DosegLeta = dosegLeta;
+        }
+        public override string ToString()
+        {
+            string ispis = "Naziv " + getNaziv() + " Snaga Motora: " + getSnagaMotora() + " Doseg Leta: " + getDosegLeta();
+            return ispis;
+        }
+    }
+}
